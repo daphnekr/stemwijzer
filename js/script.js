8506ba0d440d08f0f29046cfe6e25384037c55ba
Daphne Kramer <99046027@mydavinci.nl>
2021-02-09 20:37:48 +0100

    blauwe knop
    Als de gebruiker op een vraag komt die hij al eerder in dezelfde sessie heeft beantwoord dan wordt het laatst gegeven antwoord aangegeven als een blauwe kleur van de knop.

--- js/script.js
+++ js/script.js
@@ -5,9 +5,14 @@ var item4 = document.getElementsByClassName("item4");
 var item5 = document.getElementsByClassName("item5");
 var title = document.getElementById("title");
 var statement = document.getElementById("statement");
+var btnPro = document.getElementById("btneens").style;
+var btnContra = document.getElementById("btnoneens").style;
+var btnNone = document.getElementById("btngeen").style;
 var count = 0;
 var number = count+1;
 var arrAnswer = [];
+var answers = [];
+
 
 function Start()
 {
@@ -28,8 +33,33 @@ function SetHTMLKiesWijzer()
   statement.innerHTML = subjects[count].statement;
   item5[0].style.display = "block";
 }
+
 function SetNextStatement(count)
 {
+  for(var i = 0; i < answers.length; i++) {
+    if (answers[i].count == count) {
+      switch(answers[i].position)
+      {
+        case "pro":
+          console.log(btnPro);
+          btnPro.backgroundColor = answers[i].clicked;
+          btnContra.backgroundColor = "lightgrey";
+          btnNone.backgroundColor = "lightgrey";
+          break;
+        case "contra":
+          btnContra.backgroundColor = answers[i].clicked;
+          btnPro.backgroundColor = "lightgrey"; 
+          btnNone.backgroundColor = "lightgrey";
+          break;
+        case "none":
+          btnNone.backgroundColor = answers[i].clicked;
+          btnPro.backgroundColor = "lightgrey";
+          btnContra.backgroundColor = "lightgrey";
+          break;
+      }
+        break;
+    }
+  }
   if (subjects[count] == undefined)
   {
     GetResult(arrAnswer);
@@ -42,6 +72,7 @@ function SetNextStatement(count)
 
 function PushInArray(position)
 {
+  var exists = false;
   for (var x = 0; x < subjects[count].parties.length; x++)
   {
     if (subjects[count].parties[x].position == position)
@@ -49,10 +80,20 @@ function PushInArray(position)
       arrAnswer.push(subjects[count].parties[x].name);
     }
   }
+  for (var i = 0; i < answers.length; i++)
+  {
+    if (answers[i].count == count)
+    {
+      exists = true;
+      answers[i].position = position;
+    }
+  }
+  if(!exists) answers.push({"count": count, "position": position, "clicked": "lightblue"});
 }
 
 function AddAnswer(position)
 {
+  console.log(answers);
   PushInArray(position);  
   count++
   SetNextStatement(count);
@@ -72,6 +113,9 @@ function GetResult(finalArr)
 function GoBack()
 {
   count--;
+  console.log(count);
+  console.log(answers.length);
+
   SetNextStatement(count);
 }
 
